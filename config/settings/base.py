59cfadd870dd1c70cfc7494af0eb84e02423150d
Hamel Mesfin <hamelmesfin@gmail.com>
2024-12-19 09:24:15 -0600

    Updated production settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -342,8 +342,8 @@ CORS_URLS_REGEX = r"^/api/.*$"
 # By Default swagger ui is available only to admin user(s). You can change permission classes to change that
 # See more configuration options at https://drf-spectacular.readthedocs.io/en/latest/settings.html#settings
 SPECTACULAR_SETTINGS = {
-    "TITLE": "Django API API",
-    "DESCRIPTION": "Documentation of API endpoints of Django API",
+    "TITLE": "Django API Samples",
+    "DESCRIPTION": "Documentation of API endpoints of Django API Samples",
     "VERSION": "1.0.0",
     "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
     "SCHEMA_PATH_PREFIX": "/api/",
--- config/settings/production.py
+++ config/settings/production.py
@@ -19,7 +19,10 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["sample-api.gojjoapps.com"])
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS",
+    default=["sample-api.gojjoapps.com", "localhost:8000", "127.0.0.1:8000"],
+)
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -101,7 +104,7 @@ STATIC_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/static/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="Django API <noreply@sample-api.gojjoapps.com>",
+    default="Django API <noreply@gojjoapps.com>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
